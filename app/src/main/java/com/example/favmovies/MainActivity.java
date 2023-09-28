f1ed8eb1540a659f37f4c5684359d440ed85f656
CarolinaUniovi <uo275673@uniovi.es>
2023-09-28 13:23:20 +0200

    Terminado lab 2 con deberes para casa.

--- app/src/main/java/com/example/favmovies/MainActivity.java
+++ app/src/main/java/com/example/favmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.favmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,12 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    TODO:
+     1: validar los campos -> para guardar es obligatorio que los campos estén rellenados.
+     2: layout de editar categoria -> nombre label y edittxt, descripcion label y editTxt
+            y botón de cancel y ok.
+     */
     Snackbar msgCreaCategoria;
 
     @Override
@@ -44,12 +51,20 @@ public class MainActivity extends AppCompatActivity {
                         Snackbar.make(findViewById(R.id.layoutPrincipal),
                                         R.string.msg_accion_realizada, Snackbar.LENGTH_LONG)
                                 .show();
+                        modificarCategoria();
                     }
                 });
 
                 msgCreaCategoria.show();
             }
         });
+
+    }
+
+    private void modificarCategoria() {
+
+        Intent categoriaIntent = new Intent(MainActivity.this, CategoryActivity.class);
+        startActivity(categoriaIntent);
 
     }
 
